6b4dd8eb8b25173e909a1b3c6322945285101eb2
Manav Chhuneja <manavchhuneja@Manavs-Mac-mini.local>
2023-09-30 23:44:44 -0400

    setting up more workout templates

--- startingStrength.js
+++ startingStrength.js
@@ -47,6 +47,102 @@ const startingStrength = [
       },
     ],
   },
+  {
+    name: "Back & Biceps",
+    id: 2,
+    exercises: [
+      {
+        id: 0,
+        title: "Deadlift",
+        sets: 3,
+        reps: 8,
+      },
+      {
+        id: 1,
+        title: "Seated Row",
+        sets: 3,
+        reps: 8,
+      },
+      {
+        id: 2,
+        title: "Lat Pulldown",
+        sets: 3,
+        reps: 8,
+      },
+      {
+        id: 3,
+        title: "Bicep Curl",
+        sets: 3,
+        reps: 10,
+      },
+    ],
+  },
+  {
+    name: "Chest & Triceps",
+    id: 3,
+    exercises: [
+      {
+        id: 0,
+        title: "Bench Press",
+        sets: 3,
+        reps: 8,
+      },
+      {
+        id: 1,
+        title: "Incline Dumbell Press",
+        sets: 3,
+        reps: 8,
+      },
+      {
+        id: 2,
+        title: "Military Press",
+        sets: 3,
+        reps: 8,
+      },
+      {
+        id: 3,
+        title: "Lateral Raise",
+        sets: 3,
+        reps: 15,
+      },
+      {
+        id: 4,
+        title: "Skullcrushers",
+        sets: 3,
+        reps: 15,
+      },
+    ],
+  },
+  {
+    name: "Legs",
+    id: 4,
+    exercises: [
+      {
+        id: 0,
+        title: "Squat",
+        sets: 3,
+        reps: 5,
+      },
+      {
+        id: 1,
+        title: "Dumbell Romanian Deadlift",
+        sets: 3,
+        reps: 8,
+      },
+      {
+        id: 2,
+        title: "Leg Extension",
+        sets: 3,
+        reps: 12,
+      },
+      {
+        id: 3,
+        title: "Calf Raise",
+        sets: 3,
+        reps: 15,
+      },
+    ],
+  },
 ];
 
 // static workout template
